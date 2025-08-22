6e8724b286ec475d6bab58acbd7d682fd17a384a
Piyush <apiyushgarg2006@gmail.com>
2025-08-22 23:08:52 +0530

    drg

--- Patterns/Diamond.cpp
+++ Patterns/Diamond.cpp
@@ -63,3 +63,26 @@ int main()
     }
     return 0;
 }
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of lines you want in the pattern\n";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int spaces = abs(n / 2 - i);
+        int stars = n - 2 * spaces;
+
+        for (int j = 0; j < spaces; j++) cout << " ";
+        for (int j = 0; j < stars; j++) cout << "*";
+        cout << "\n";
+    }
+
+    return 0;
+}
