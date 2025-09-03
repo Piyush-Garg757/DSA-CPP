a481cd84fbbd43ac474b468b79da530b7e8cd4e6
Piyush <apiyushgarg2006@gmail.com>
2025-09-03 19:01:53 +0530

    wf

--- Array/2-D/Set_matrix_zeroes.cpp
+++ Array/2-D/Set_matrix_zeroes.cpp
@@ -118,3 +118,78 @@ int main()
 
 // Optimal solution
 // TC to kam kar nahi sakte kyuki 2d array hai   sc ko hi kam kar sakte hain
+// ab tak jo do separate arrays le rahe the unhein ab matrix ki first row aur first column lenge
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cout << "Enter the number of rows\n";
+    cin >> n;
+    cout << "Enter the number of columns\n";
+    cin >> m;
+    int a[n][m];
+    cout << "Enter the elements of the matrix\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout << "\n";
+    int col0 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 0)
+            {
+                a[i][0] = 0;
+                if (j != 0)
+                {
+                    a[0][j] = 0;
+                }
+                else
+                {
+                    col0 = 0;
+                }
+            }
+        }
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (a[i][0] == 0 || a[0][j] == 0)
+            {
+                a[i][j] = 0;
+            }
+        }
+    }
+    if (a[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            a[0][j] = 0;
+        }
+    }
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            a[i][0] = 0;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// TC - O(n^2)  SC - O(1)
