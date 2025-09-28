405725bed801ee105e6f26fa887f68c617bed741
Piyush <apiyushgarg2006@gmail.com>
2025-09-28 17:46:13 +0530

    fg

---
+++ Leetcode/Easy/27.cpp
@@ -0,0 +1,22 @@
+/*Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k*/
+class Solution
+{
+public:
+    int removeElement(vector<int> &a, int k)
+    {
+        int j = 0;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] != k)
+            {
+                a[j++] = a[i];
+            }
+        }
+        return j;
+    }
+};
+// TC - O(n)  SC - O(1)
