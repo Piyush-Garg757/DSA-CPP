83fe1fb1f568ea342a9ad091718abaef8690a3ea
Piyush <apiyushgarg2006@gmail.com>
2025-09-29 17:57:04 +0530

    fg

---
+++ Leetcode/Easy/219.cpp
@@ -0,0 +1,30 @@
+/* Contains Duplicate II
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false*/
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> lastSeen;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (lastSeen.count(nums[i]) && i - lastSeen[nums[i]] <= k)
+                return true;
+            lastSeen[nums[i]] = i;
+        }
+        return false;
+    }
+};
+// TC - O(n)  SC - O(n)
