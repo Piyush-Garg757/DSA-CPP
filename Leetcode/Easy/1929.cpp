8283b2384f07d340f827fda9fca0c0055771f3d1
Piyush <apiyushgarg2006@gmail.com>
2025-10-04 16:26:47 +0530

    qefvafsdg

---
+++ Leetcode/Easy/1929.cpp
@@ -0,0 +1,29 @@
+/*Concatenation of Array
+
+Example 1:
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+
+Example 2:
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]*/
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
+// TC - O(n)  SC - O(n)
