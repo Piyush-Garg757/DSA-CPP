692732f67981b1509ab246fc6926f9b7d7307c21
Piyush <apiyushgarg2006@gmail.com>
2025-10-29 15:11:48 +0530

    xfb

---
+++ Leetcode/Easy/2535.cpp
@@ -0,0 +1,35 @@
+/*Difference Between Element Sum and Digit Sum of an Array
+You are given a positive integer array nums.
+The element sum is the sum of all the elements in nums.
+The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+Return the absolute difference between the element sum and digit sum of nums.
+Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+Example 1:
+
+Input: nums = [1,15,6,3]
+Output: 9
+Explanation:
+The element sum of nums is 1 + 15 + 6 + 3 = 25.
+The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+The absolute difference between the element sum and digit sum is |25 - 16| = 9.*/
+class Solution
+{
+public:
+    int differenceOfSum(vector<int> &nums)
+    {
+        int elementSum = 0, digitSum = 0;
+        for (int n : nums)
+        {
+            elementSum += n;
+            int temp = n;
+            while (temp > 0)
+            {
+                digitSum += temp % 10;
+                temp /= 10;
+            }
+        }
+        return abs(elementSum - digitSum);
+    }
+};
+// TC - O(n^2)  SC - O(1)
