6e186fa907587ee999ca6655a2085b627a53cc46
Piyush <apiyushgarg2006@gmail.com>
2025-11-11 19:45:18 +0530

    Added a question

---
+++ Leetcode/Practice/Easy/867.cpp
@@ -0,0 +1,20 @@
+/*Transpose Matrix*/
+class Solution
+{
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &a)
+    {
+        int n = a.size();
+        int m = a[0].size();
+        vector<vector<int>> ans(m, vector<int>(n));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                ans[j][i] = a[i][j];
+            }
+        }
+        return ans;
+    }
+};
+// TC - O(n^2)  SC - O(n^2)
