36aed10ed943a892d8a506f8c81831e4ffa7d13a
Piyush <apiyushgarg2006@gmail.com>
2025-12-20 17:29:07 +0530

    Added a questiom

---
+++ Leetcode/Practice/Medium/53.cpp
@@ -0,0 +1,38 @@
+/*Maximum Subarray
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23*/
+
+// Brute force aur beter to wahi nested loops vale
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &a)
+    {
+        int n = a.size(), ans = INT_MIN, sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += a[i];
+            ans = max(ans, sum);
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(1)
