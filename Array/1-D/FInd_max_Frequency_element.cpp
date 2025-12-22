b4608aa6d3e1fecd2d601e3a4d6296544bd644b5
Piyush <apiyushgarg2006@gmail.com>
2025-12-22 19:39:11 +0530

    Added a concept

--- Array/1-D/FInd_max_Frequency_element.cpp
+++ Array/1-D/FInd_max_Frequency_element.cpp
@@ -92,3 +92,95 @@ int main()
     return 0;
 }
 // Time Complexity: O(N) + O(N) = O(N)  SC - O(1)
+
+// Ishi mein ek variety ki jitne bhi number floor(n/3) se zyada times aaye unhein print karna hai
+// to ye at max 2 aa sakte hain ek array mein
+/*for eg 1 1 1 2 2 3 3 3
+floor(n/3) yani floor(8/3)  yani 2
+to aise elements jo min 3 times aaye to clearly ismein 2 hi ho sakte hain
+
+To brute force to yahi ki o(n^2) har number ke liye check karenge kitni baar aa raha hai aur uske according answer nikalenge*/
+
+// Better sol -> hashing through map
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array\n";
+    cin >> n;
+    int a[n];
+    unordered_map<int, int> mpp;
+    cout << "Enter the elements of the array\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mpp[a[i]]++;
+    }
+    int check = n / 3;
+    for (auto it : mpp)
+    {
+        if (it.second > check)
+            cout << it.first << " ";
+    }
+    return 0;
+}
+// TC - O(n)  SC - O(n)
+
+// Optimal sol -> Moore's Voting Algo hi
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array\n";
+    cin >> n;
+    int a[n];
+    cout << "Enter the elements of the array\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int cnt1 = 0, cnt2 = 0;
+    int el1 = 0, el2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == el1)
+            cnt1++;
+        else if (a[i] == el2)
+            cnt2++;
+        else if (cnt1 == 0)
+        {
+            el1 = a[i];
+            cnt1 = 1;
+        }
+        else if (cnt2 == 0)
+        {
+            el2 = a[i];
+            cnt2 = 1;
+        }
+        else
+        {
+            cnt1--;
+            cnt2--;
+        }
+    }
+    cnt1 = cnt2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == el1)
+            cnt1++;
+        else if (a[i] == el2)
+            cnt2++;
+    }
+    cout << "Majority elements (> n/3):\n";
+    if (cnt1 > n / 3)
+        cout << el1 << " ";
+    if (cnt2 > n / 3)
+        cout << el2 << " ";
+    return 0;
+}
+// TC - O(n)  SC - O(1)
