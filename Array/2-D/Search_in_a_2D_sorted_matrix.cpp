a1b637f89f520cb1c3b2ac48f9b906c4a524161b
Piyush <apiyushgarg2006@gmail.com>
2025-12-28 18:43:56 +0530

    Added a question

--- Array/2-D/Search_in_a_2D_sorted_matrix.cpp
+++ Array/2-D/Search_in_a_2D_sorted_matrix.cpp
@@ -1,3 +1,4 @@
+// Matrix sorted hai matlab ek row ka last number agli row ke first number se chota hai
 class Solution
 {
 public:
@@ -25,3 +26,43 @@ public:
     }
 };
 // TC - O(log(n+m))  SC - O(1)
+
+// rows aur columns dono sorted hain
+
+/*Intution
+1 2 3
+2 4 5
+4 6 8
+
+sarting from upper 3 as it is largest in row but smallest in column so if we want to find 6 we ca eliminate this row as largest is smaller than k  similarly we can eliminate columns*/
+
+class Solution
+{
+public:
+    vector<int> searchMatrix(vector<vector<int>> &mat, int target)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0)
+        {
+            if (mat[row][col] == target)
+            {
+                return {row, col};
+            }
+            else if (mat[row][col] < target)
+            {
+                row++;
+            }
+            else
+            {
+                col--;
+            }
+        }
+        return {-1, -1}; 
+    }
+};
+// TC - O(n+m)  SC - O(1)
