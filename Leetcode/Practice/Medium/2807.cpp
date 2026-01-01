9d54b6dc1308b73cbea920921f1ef5f61bd9c342
Piyush <apiyushgarg2006@gmail.com>
2026-01-01 23:45:04 +0530

    Added

---
+++ Leetcode/Practice/Medium/2807.cpp
@@ -0,0 +1,61 @@
+/*Insert Greatest Common Divisors in Linked List
+Given the head of a linked list head, in which each node contains an integer value.
+Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+Return the linked list after insertion.
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+Example 1:
+Input: head = [18,6,10,3]
+Output: [18,6,6,2,10,1,3]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+- We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+- We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+- We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+There are no more adjacent nodes, so we return the linked list.
+
+Example 2:
+Input: head = [7]
+Output: [7]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+There are no pairs of adjacent nodes, so we return the initial linked list.*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int find_hcf(int a, int b)
+    {
+        while (b != 0)
+        {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+    ListNode *insertGreatestCommonDivisors(ListNode *head)
+    {
+        ListNode *temp = head;
+        ListNode *temp2 = head->next;
+        while (temp2)
+        {
+            ListNode *newnode = new ListNode(find_hcf(temp->val, temp2->val));
+            temp->next = newnode;
+            newnode->next = temp2;
+            temp = temp2;
+            temp2 = temp2->next;
+        }
+        return head;
+    }
+};
+
+// TC - O(nlog(min(a,b)))  SC - O(1)
