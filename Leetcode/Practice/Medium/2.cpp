3641a507a5f132c2d9b1e55c4d412157d36c6587
Piyush <apiyushgarg2006@gmail.com>
2026-01-06 20:22:17 +0530

    Added concepts

--- Leetcode/Practice/Medium/2.cpp
+++ Leetcode/Practice/Medium/2.cpp
@@ -62,4 +62,4 @@ public:
 };
 // TC - O(n)  SC - O(1)
 
-// Ismien dummy node lene se initialisatin vagera mein fayda rehta hai isiliye liya hai  to aage dhyaan rakhn agar alag hi linked list banani hai to dummy node leke karna
+// Ismien dummy node lene se initialisatin vagera mein fayda rehta hai isiliye liya hai  to aage dhyaan rakhn agar alag hi linked list banani hai to dummy node leke karna hai
