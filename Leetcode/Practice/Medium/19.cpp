491dc17f8c5a216cc1d9291d47770ea58f62e1d1
Piyush <apiyushgarg2006@gmail.com>
2026-01-07 13:16:22 +0530

    Added concepts

--- Leetcode/Practice/Medium/19.cpp
+++ Leetcode/Practice/Medium/19.cpp
@@ -56,3 +56,43 @@ public:
     }
 };
 // TC - O(n)  SC - O(1)
+
+// Optimal  ismein ek hi baar iterate hoga
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        for (int i = 0; i < n; i++)
+            fast = fast->next;
+        if (fast == nullptr)
+        {
+            ListNode *newnode = head;
+            head = head->next;
+            delete newnode;
+            return head;
+        }
+        while (fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode *newnode = slow->next;
+        slow->next = slow->next->next;
+        delete newnode;
+        return head;
+    }
+};
+// TC - O(n)  SC - O(1)
