e70a020e4e2b588da5c759a4407d07d2b32cca23
Piyush <apiyushgarg2006@gmail.com>
2026-01-11 16:27:51 +0530

    Added concepts

---
+++ Leetcode/Practice/Easy/3794.cpp
@@ -0,0 +1,38 @@
+/*Reverse String Prefix
+You are given a string s and an integer k.
+Reverse the first k characters of s and return the resulting string.
+
+Example 1:
+Input: s = "abcd", k = 2
+Output: "bacd"
+Explanation:​​​​​​​
+The first k = 2 characters "ab" are reversed to "ba". The final resulting string is "bacd".
+
+Example 2:
+Input: s = "xyz", k = 3
+Output: "zyx"
+Explanation:
+The first k = 3 characters "xyz" are reversed to "zyx". The final resulting string is "zyx".
+
+Example 3:
+Input: s = "hey", k = 1
+Output: "hey"
+Explanation:
+The first k = 1 character "h" remains unchanged on reversal. The final resulting string is "hey".*/
+
+class Solution
+{
+public:
+    string reversePrefix(string s, int k)
+    {
+        int i = 0;
+        while (i < k - 1)
+        {
+            swap(s[i], s[k - 1]);
+            i++;
+            k--;
+        }
+        return s;
+    }
+};
+// TC - O(n)  SC - O(1)
