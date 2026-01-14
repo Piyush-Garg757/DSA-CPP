496f46c49b93761847bb33c26631ad83ab779390
Piyush <apiyushgarg2006@gmail.com>
2026-01-14 15:31:14 +0530

    Added concepts

--- Linked List/Singly/Starting_node_of_a_cycle.cpp
+++ Linked List/Singly/Starting_node_of_a_cycle.cpp
@@ -40,3 +40,34 @@ public:
     }
 };
 // TC - O(n)  SC - O(1)
+
+/*
+two questions arise
+    Q1. Why are w esure they will collide ?
+    Q2. How are we sure they collide at the starting point?
+    
+    Distance notation:
+head → x → [ LOOP START ] → y → [ MEETING POINT ]
+                         ↖────── c ──────↙
+x = head se loop start tak
+y = loop start se meeting tak
+c = loop length
+Movement math:
+Slow travelled = x + y
+Fast travelled = x + y + c
+But:
+fast = 2 × slow
+So:
+2(x + y) = x + y + c
+x + y = c
+x = c − y
+🔥 Key insight
+Meeting point se loop start tak distance = c − y
+Head se loop start tak distance = x
+And:
+x = c − y
+👉 Matlab:
+Ek pointer head se chalao
+Ek pointer meeting point se
+Same speed (1 step)
+✅ Dono exactly loop ke starting node pe milenge*/
