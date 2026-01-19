fb8a125e18baa10ab74140eda1342c29bee458d0
Piyush <apiyushgarg2006@gmail.com>
2026-01-19 10:52:29 +0530

    Added concepts

--- Leetcode/Practice/Easy/1290.cpp
+++ Leetcode/Practice/Easy/1290.cpp
@@ -57,3 +57,31 @@ public:
     }
 };
 // TC - O(n)  SC - O(1)
+
+// Best approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int getDecimalValue(ListNode *head)
+    {
+        int ans = 0;
+        while (head)
+        {
+            ans = (ans << 1) | head->val;
+            head = head->next;
+        }
+        return ans;
+    }
+};
+// TC - O(n)  SC - O(1)
